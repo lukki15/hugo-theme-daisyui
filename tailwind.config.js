1288548b3fda6f7372525a3a1296a0666ba4b757
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-02-14 18:59:33 +0100

    only use dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 }
 
